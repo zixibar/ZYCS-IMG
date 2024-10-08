7363eb9dbfb20535f94e8236ad69fabd2c9ed1d2
zixibar <76789158+zixibar@users.noreply.github.com>
2024-10-08 21:41:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <a-watermark class="page_main" content="骤雨重山图床" :alpha="0.1">
+  <a-watermark class="page_main" content="svip村图床" :alpha="0.1">
     <header>
-      <h1>骤雨重山图床</h1>
+      <h1>svip村图床</h1>
       <h2>致力于为开发者提供便捷、免费、稳定、快速的图床服务。</h2>
       <div class="desc">
         <p>无限图片储存数量，你可以上传不限数量的图片！</p>
         <p>图片首次访问后缓存，"永久"有效，包括全球分布的 CDN，以确保尽可能快地提供图像.</p>
         <p>项目使用无服务器服务搭建，托管在 <a-tag color="gray"><template #icon><icon-cloud /></template>Cloudflare Pages</a-tag> 上运行.
         </p>
-        <p>开源地址： <a-link href="https://github.com/uxiaohan/ZYCS-IMG" target="_blank" icon>ZYCS-IMG</a-link></p>
       </div>
     </header>
     <main>
@@ -74,7 +73,7 @@ const ACTION_API = import.meta.env.VITE_IMG_API_URL;
 const fileName = ref('');
 const checkImageSizeFn = (file: any) => {
   fileName.value = file.name
-  const maxSize = 5 * 1024 * 1024;
+  const maxSize = 10 * 1024 * 1024;
   if (file.size > maxSize) {
     Message.error(`图片大小不能超过 ${maxSize / 1024 / 1024}MB`);
     return false;
